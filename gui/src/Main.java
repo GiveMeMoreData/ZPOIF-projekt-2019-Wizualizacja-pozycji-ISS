5a0c2f4630850393eeb8b78f533c0aed19514a77
Bartlomiej Eljasiak <Bartlomiej_Eljasiak@users.noreply.github.com>
2020-01-27 14:41:52 +0100

    gui final update

--- gui/src/Main.java
+++ gui/src/Main.java
@@ -19,11 +19,13 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Popup;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import netscape.javascript.JSObject;
+
 
 import java.awt.*;
 import java.io.FileInputStream;
@@ -38,15 +40,21 @@ public class Main extends Application {
 
     private JSObject javascriptConnector;
 
-    //TODO Tu będą wszystkie wartości ustawień, domyślnie zczytywane z pliku przy uruchomieniu aplikacji
+    // Parametry domyślne, normalnie ładowane z pliku config przez readProperties()
     private int windowHeight = 1080;
     private int windowWidth = 1920;
-
     private boolean fullscreen = true;
     private String theme = "Light";
 
+
+    // Odczyt rozdzielczość ekranu użytkownika
+    Rectangle2D screen = Screen.getPrimary().getBounds();
+    int screenWidth = (int) screen.getWidth();
+    int screenHeight = (int) screen.getHeight();
+
+    //Wszystkie sceny aplikacji
     Stage stage;
-    Scene menu,loading,map,issLiveView,settings;
+    Scene menu,loading,map,settings;
 
     public void start(Stage unusedStage) {
         // 5 słów wyjaśnienia
@@ -58,26 +66,26 @@ public class Main extends Application {
         //           Jej konstruktor wymaga layoutu i zdefinowanej wysokości i szerokości
         // Stage  <- fundament aplikacji, mu będziemy dawać różne sceny do wyświetlenia
 
+        //Wczytaj domyśle ustawienia
+        readProperties();
+
+
+        // Główne okno aplikacji
         stage = new Stage();
         stage.setWidth(windowWidth);
         stage.setHeight(windowHeight);
         stage.setFullScreenExitHint("");
         stage.getIcons().add(new Image("icon.png"));
 
-        //Loading saved properties
-        readProperties();
-
+        // Tworzenie wszystkich scen aplikacji
         loading = new Scene(getLoadingLayout());
         menu = new Scene(getMenuLayout());
         map = new Scene(getMapLayout());
-
-        issLiveView = new Scene(getIssLiveViewLayout());
         settings = new Scene(getSettingsLayout());
 
-
+        // Animacja ekranu ładowania
         PauseTransition timer = new PauseTransition(Duration.millis(1500));
         timer.setOnFinished(actionEvent -> setScene(menu));
-
 
         // Tu następuje pojawienie się aplikacji
         stage.setScene(loading);
@@ -87,50 +95,59 @@ public class Main extends Application {
         timer.play();
     }
 
-
     private Parent getSettingsLayout() {
-        // LAYOUT ustawien
+        // LAYOUT ustawień
 
         HBox layoutSettings = new HBox(100);
+        layoutSettings.setAlignment(Pos.CENTER);
 
         VBox settingsViews = new VBox(40);
         settingsViews.setAlignment(Pos.CENTER);
 
         HBox settingsGeneralControls = new HBox(80);
-        VBox settingsGeneralLabels = new VBox(40);
-        settingsGeneralLabels.setAlignment(Pos.CENTER);
+        VBox settingsGeneralLabels = new VBox(56);
+        settingsGeneralLabels.setAlignment(Pos.CENTER_LEFT);
         VBox settingsGeneralValues = new VBox(40);
         settingsGeneralValues.setAlignment(Pos.CENTER);
 
 
-        // Ustawienie Boxów
+        // Ustawienie kontenerów
         layoutSettings.getChildren().addAll(settingsViews,settingsGeneralControls);
+
+        //Dodanie pola na opisy ustawień i kontrolki
         settingsGeneralControls.getChildren().addAll(settingsGeneralLabels,settingsGeneralValues);
 
-
-
+        //Przyciski powrotu i zmiany ustawień
         Button btnAcceptChanges = new Button("Akceptuj");
         Button btnBack = new Button("Wróć");
 
+        settingsViews.getChildren().addAll(btnBack,btnAcceptChanges);
+        btnBack.setOnAction(actionEvent -> setScene(menu));
 
+        /*
+            USTAWIENIA
+        */
+
+        // Zmiana rozdzielczości
         ComboBox<String> resolutionComboBox = new ComboBox<>();
         resolutionComboBox.getItems().addAll(
                 "1920x1080",
-                "1680x1050",
-                "1600x1200",
-                "1440x960",
-                "1400x1050",
-                "1280x720"
+                    "1776x1000",
+                    "1760x990",
+                    "1600x1024",
+                    "1600x900",
+                    "1440x960",
+                    "1400x1050"
         );
-        resolutionComboBox.getSelectionModel().selectFirst();
+        resolutionComboBox.setValue(windowWidth +"x"+ windowHeight);
 
+        // Wybór motywu aplikacji
         ComboBox<String> themeComboBox = new ComboBox<>();
-        themeComboBox.getItems().addAll(
-                "Light",
-                "Dark"
-        );
-        themeComboBox.getSelectionModel().selectFirst();
-        Button btnViewSourceCode = new Button("Source Code");
+        themeComboBox.getItems().addAll("Light","Dark");
+        themeComboBox.setValue(theme);
+
+        //Otworzenie okna w przeglądarce z kodem źródłowym
+        Button btnViewSourceCode = new Button("Github");
         btnViewSourceCode.setOnAction(actionEvent -> {
             try {
                 Desktop.getDesktop().browse(new URL("https://github.com/GiveMeMoreData/ZPOIF-projekt-2019-Wizualizacja-pozycji-ISS").toURI());
@@ -139,58 +156,71 @@ public class Main extends Application {
             }
         });
 
+        //Checkbox trybu pełnoekranowego
         CheckBox fullscreenCheckBox = new CheckBox();
+        fullscreenCheckBox.setSelected(fullscreen);
 
+        /*
+            POPUP zmianny ustawień
+        */
 
-        //Zmiana ustawień
-        btnAcceptChanges.setOnAction(actionEvent -> {
+        Popup popup = new Popup();
 
+        // Elementy na popup'ie
+        Label text = new Label("Wprowadzenie zmian wymaga\nponownego uruchomienia aplikacji");
+        text.setAlignment(Pos.CENTER);
+
+        Button cancelBtn = new Button("Anuluj");
+        cancelBtn.setOnAction(actionEvent -> popup.hide());
+
+        Button restartBtn = new Button("Restartuj");
+        restartBtn.setOnAction(actionEvent -> {
             if( fullscreenCheckBox.isSelected()!=fullscreen){
-                fullscreen=fullscreenCheckBox.isSelected();
-                stage.setMaximized(fullscreen);
-
+                fullscreen=!fullscreen;
+                windowWidth=screenWidth;
+                windowHeight=screenHeight;
             }
             if (!fullscreen){
                 String[] values = resolutionComboBox.getValue().split("x");
                 setStageSize(Integer.parseInt(values[0]),Integer.parseInt(values[1]));
             }
-
-            theme= themeComboBox.getValue();
-            setScene(settings);
-
+            saveProperties();
+            Platform.exit();
+            System.exit(0);
         });
 
+        // Layout popup'a
+        HBox h = new HBox(40,cancelBtn,restartBtn);
+        VBox v =  new VBox(40,text,h);
+        v.setAlignment(Pos.CENTER);
+        h.setAlignment(Pos.CENTER);
 
+        StackPane st = new StackPane(v);
+        st.getStylesheets().setAll("popup.css");
+        st.setStyle("-fx-background-color: lightgrey; -fx-padding: 20;");
 
-        settingsGeneralLabels.getChildren().addAll(new Label("Rozdzielczość"),new Label("Fullscreen"),new Label("Theme"), new Label("View Source\nCode"));
+        // Zatwierdzenie kontenerów i elementów w popup'ie
+        popup.getContent().add(st);
+
+        //Zmiana ustawień
+        btnAcceptChanges.setOnAction(actionEvent -> {
+            //Zmiany wymagające restartu
+            if (fullscreenCheckBox.isSelected()!=fullscreen || !resolutionComboBox.getValue().equals(windowWidth +"x"+ windowHeight)){
+                popup.show(stage);
+            }
+
+            //Zmiany nie wymagające restaru
+            theme= themeComboBox.getValue();
+            setScene(settings);
+        });
+
+        // Dodanie wszystkich napisów i kontrolek ustawień do odpowiednich kontenerów
+        settingsGeneralLabels.getChildren().addAll(new Label("Rozdzielczość"),new Label("Fullscreen"),new Label("Theme"), new Label("Zobacz projekt\nna Github"));
         settingsGeneralValues.getChildren().addAll(resolutionComboBox,fullscreenCheckBox,themeComboBox,btnViewSourceCode);
 
-        settingsViews.getChildren().addAll(btnBack,btnAcceptChanges);
-
-
-        btnBack.setOnAction(actionEvent -> setScene(menu));
 
         return layoutSettings;
 
-    }
-
-    private Parent getIssLiveViewLayout() {
-        // LAYOUT ISS Livestream
-
-
-
-        VBox layoutIssLive = new VBox();
-        WebView issWebView = new WebView();
-        // Ten link powinien być ok ale film nie chce się ładować, może chodzić o silnik przeglądarki, którego używa javafx
-        //https://www.youtube.com/embed/kL090M6qtXQ?controls=0&autoplay=1&fs=0&mute=1&iv_load_policy=3&rel=0&showinfo=0
-        issWebView.getEngine().load("https://www.youtube.com/embed/kL090M6qtXQ?controls=0&autoplay=1&fs=0&mute=1&iv_load_policy=3&rel=0&showinfo=0");
-        issWebView.setPrefSize(windowWidth,windowHeight);
-
-        Button btnBack = new Button("Wróć");
-        btnBack.setOnAction(actionEvent -> setScene(menu));
-
-        layoutIssLive.getChildren().addAll(issWebView,btnBack);
-        return layoutIssLive;
     }
 
     private Parent getMenuLayout(){
@@ -205,10 +235,17 @@ public class Main extends Application {
         Button btnExit = new Button("Zamknij");
         layoutMenu.getChildren().addAll(btnMap, btnIssLive, btnSettings, btnExit);
 
+        btnIssLive.setOnAction(actionEvent -> {
+            try {
+                Desktop.getDesktop().browse(new URL("https://www.youtube.com/embed/wmHF9aLVOeM?controls=0&autoplay=1&fs=0&mute=1&iv_load_policy=3&rel=0&showinfo=0").toURI());
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
+
+
         // Przejście do mapy (webview)
         btnMap.setOnAction(actionEvent -> setScene(map));
-
-        btnIssLive.setOnAction(actionEvent -> setScene(issLiveView));
 
         // Przejście do ustawień
         btnSettings.setOnAction(actionEvent -> setScene(settings));
@@ -223,50 +260,20 @@ public class Main extends Application {
         return layoutMenu;
     }
 
-    private void readProperties(){
-        Properties prop = new Properties();
-
-        try {
-            prop.load(new FileInputStream("config.properties"));
-        } catch (IOException e) {
-            System.out.println("File seems to not exist");
-            e.printStackTrace();
-        }
-
-        fullscreen =  Boolean.parseBoolean(prop.getProperty("fullscreen"));
-        theme =prop.getProperty("theme");
-
-    }
-
-    private void saveProperties() {
-        Properties prop = new Properties();
-
-        try {
-            //set the properties value
-            prop.setProperty("fullscreen", String.valueOf(fullscreen));
-            prop.setProperty("theme", theme);
-
-            //save properties to project root folder
-            prop.store(new FileOutputStream("config.properties"), null);
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     private Parent getMapLayout() {
         // Map
         VBox layoutMap = new VBox();
         WebView webView = new WebView();
-        final WebEngine webEngine = webView.getEngine();
+        webView.setContextMenuEnabled(false);
+        WebEngine webEngine = webView.getEngine();
         webView.setPrefSize(5000,5000);
 
 
         Button btnBackMap = new Button("Wróć");
         layoutMap.getChildren().addAll(webView, btnBackMap);
 
-        String background = "-fx-background-color: #1A233B;";
-        layoutMap.setStyle(background);
+
+        layoutMap.setStyle("-fx-background-color: #1A233B;");
 
         webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
             if (Worker.State.SUCCEEDED == newValue) {
@@ -296,15 +303,18 @@ public class Main extends Application {
                 sv.start();
             }
         });
+
+        webEngine.executeScript("window.width="+(windowWidth*0.85));
         URL url = this.getClass().getResource("webview.html");
         webEngine.load(url.toString());
-        webEngine.executeScript("var width="+(windowWidth-20));
+
 
         btnBackMap.setOnAction(actionEvent -> setScene(menu));
 
         return layoutMap;
     }
 
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
     private Parent getLoadingLayout() {
         //Loading screen layout
         Image loadingImage = new Image("face.png",windowWidth/2,windowHeight/2,true,true);
@@ -331,13 +341,44 @@ public class Main extends Application {
     private void setStageSize(int width, int height){
         windowHeight=height;
         windowWidth=width;
-        Rectangle2D screen = Screen.getPrimary().getBounds();
-        double screenWidth = screen.getWidth();
-        double screenHeight = screen.getHeight();
-        stage.setHeight(windowHeight);
-        stage.setWidth(windowWidth);
-        stage.setY((screenHeight-windowHeight)/2);
-        stage.setX((screenWidth-windowWidth)/2);
+    }
+
+    private void readProperties(){
+        // Odczytuje zapisane ustawienia z pliku config.properties
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            System.out.println("File seems to not exist");
+            e.printStackTrace();
+        }
+
+        // Przypisanie wszystkich zapisanych ustawień
+        // Do zmiennych używanych przez aplikacje
+        fullscreen =  Boolean.parseBoolean(prop.getProperty("fullscreen"));
+        theme =prop.getProperty("theme");
+        windowWidth =Integer.parseInt(prop.getProperty("windowWidth"));
+        windowHeight = Integer.parseInt(prop.getProperty("windowHeight"));
+
+    }
+
+    private void saveProperties() {
+        Properties prop = new Properties();
+
+        try {
+            //set the properties value
+            prop.setProperty("fullscreen", String.valueOf(fullscreen));
+            prop.setProperty("theme", theme);
+            prop.setProperty("windowWidth", String.valueOf(windowWidth));
+            prop.setProperty("windowHeight", String.valueOf(windowHeight));
+
+            //save properties to project root folder
+            prop.store(new FileOutputStream("config.properties"), null);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
